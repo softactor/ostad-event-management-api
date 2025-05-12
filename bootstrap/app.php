87c253a529069be66e7c302a783a460e886a9b82
softactor <tanveerqureshee1@gmail.com>
2025-05-12 07:48:28 +0000

    private channel add

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\BroadcastAuthenticate;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,7 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Add middleware for the API group
+        $middleware->group('api', [
+            BroadcastAuthenticate::class, // Your custom Bearer authentication middleware
+            'throttle:api',  // Default API throttling middleware
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
